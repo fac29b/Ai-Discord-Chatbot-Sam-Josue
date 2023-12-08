7de8958f3816f5b9fd2122edced1c8204d41aa8d
sam hockaday <samuel.hockaday@gmail.com>
2023-12-08 11:45:15 +0000

    I removed the else statement from above try. Try was not triggering because of else.

--- bot.js
+++ bot.js
@@ -14,104 +14,66 @@ let beginner = "beginner";
     const intermediateMessage = `In this chat, do not provide any explanations of code. Only use single-letter variable names. Generate 1 example of a modern JavaScript code-reading challenge you might get in a job interview. The difficulty level should be ${intermediate} For these examples, use a mixture of different array methods.`;
     const advancedMessage = `In this chat, do not provide any explanations of code. Only use single-letter variable names. Generate 1 example of a modern JavaScript code-reading challenge you might get in a job interview. The difficulty level should be ${advanced} For these examples, use a mixture of different array methods.`;
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
 
-const openai = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY,
-});
-
-client.once('ready', () => {
- 
-  console.log('Bot is online');
-});
-
-
-
-
-
-//WHEN YOU ENTER !codeChallenge IT TRIGGERS THE CODE BOT. 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
-
-// history goes here
-    let prevMessages = await message.channel.messages.fetch({ limit: 12 });
-    prevMessages = prevMessages.reverse();
-    await message.channel.sendTyping()
-
-    prevMessages.forEach((msg) => {
-      if (!msg.author.bot || msg.author.id === client.user.id) {
-        conversation.push({
-          role: msg.author.id === client.user.id ? 'assistant' : 'user',
-          content: msg.content,
-        });
-      }
+    const client = new Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+      ],
     });
-// 
-
-  if (message.content === '!intermediate') {
-    conversation.push({
-      role: 'user',
-      content: intermediateMessage,
+    
+    const openai = new OpenAI({
+      apiKey: process.env.OPEN_API_KEY,
     });
-
-    console.log(conversation)
-
-    await message.channel.sendTyping()
-  } else {
- // NORMAL AI CHATBOT FUNCTIONALITY
     
-
-  
-    
-    
-
-    let prevMessages = await message.channel.messages.fetch({ limit: 12 });
-    prevMessages = prevMessages.reverse();
-    await message.channel.sendTyping()
-
-    prevMessages.forEach((msg) => {
-      if (!msg.author.bot || msg.author.id === client.user.id) {
-        conversation.push({
-          role: msg.author.id === client.user.id ? 'assistant' : 'user',
-          content: msg.content,
-        });
-      }
+    client.once('ready', () => {
+      console.log('Bot is online');
     });
-  
-    // conversation.push({
-    //   role: 'user',
-    //   content: firstMessge,
-    // });
-
-    console.log(typeof userMessage);
-
-    try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: conversation,
-        stream: true,
+    
+    client.on('messageCreate', async (message) => {
+      if (message.author.bot) return;
+    
+      let prevMessages = await message.channel.messages.fetch({ limit: 12 });
+      prevMessages = prevMessages.reverse();
+      await message.channel.sendTyping();
+    
+      prevMessages.forEach((msg) => {
+        if (!msg.author.bot || msg.author.id === client.user.id) {
+          conversation.push({
+            role: msg.author.id === client.user.id ? 'assistant' : 'user',
+            content: msg.content,
+          });
+        }
       });
-
-     let botReply = '';
-      for await (const chunk of response) {
-        botReply += chunk.choices[0]?.delta?.content || '';
+    
+      if (message.content === '!intermediate') {
+        conversation.push({
+          role: 'user',
+          content: intermediateMessage,
+        });
       }
- 
-      if (botReply) {
-        message.reply(botReply.trim());
+    
+      // Common logic for handling both special commands and regular messages
+      try {
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: conversation,
+          stream: true,
+        });
+    
+        let botReply = '';
+        for await (const chunk of response) {
+          botReply += chunk.choices[0]?.delta?.content || '';
+        }
+    
+        if (botReply) {
+          message.reply(botReply.trim());
+        }
+      } catch (error) {
+        console.error('OpenAI Error:', error.message);
+        message.reply("Apologies, but I'm unable to respond right now.");
       }
-    } catch (error) {
-      console.error('OpenAI Error:', error.message);
-      message.reply("Apologies, but I'm unable to respond right now.");
-    }
-  }
-});
-
-client.login(process.env.DISCORD_TOKEN);
-//END
+    });
+    
+    client.login(process.env.DISCORD_TOKEN);
